be07e725a829ef1b906cd7a5324c5272dfd0f0dc
Remko Silvis <remko.silvis@mediact.nl>
2025-07-21 14:16:47 +0200

    Update publish script

--- github-publish.sh
+++ github-publish.sh
@@ -3,4 +3,5 @@
 # Effectively publishing the page as it is in the /public folder.
 # to force it in case of error, this can be used:
 # git push origin `git subtree split --prefix public master`:gh-pages --force
+
 git subtree push --prefix public origin gh-pages
